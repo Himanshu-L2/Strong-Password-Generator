e68e07c51e33f6c70805b036583457ad99f4d107
Himanshu-L2 <76200927+Himanshu-L2@users.noreply.github.com>
2020-12-21 11:18:57 +0530

    Add files via upload
    First commit

---
+++ Password_Gene.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+s1=string.ascii_lowercase
+s2=string.ascii_uppercase
+s3=string.digits
+s4=string.punctuation
+
+plen=int(input('Please Enter Password Length'))
+s=[]
+s.extend(list(s1))
+s.extend(list(s2))
+s.extend(list(s3))
+s.extend(list(s4))
+random.shuffle(s)
+print("".join(s[0:plen]))
